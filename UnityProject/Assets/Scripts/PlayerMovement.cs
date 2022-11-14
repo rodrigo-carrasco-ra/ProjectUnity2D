0199e58ea3e781c692d1a0a1e58804f5e143f20c
Rodrigo Carrasco <rodrigo.carrasco40@inacapmail.cl>
2022-11-13 22:49:50 -0300

    Avance lvl 1

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float velocidad;
-    private Rigidbody2D rigidBody;
-    private bool mirandoDerecha = true;
-    // Start is called before the first frame update
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+ 
     void Start()
     {
-        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ProcesarMovimiento();
+        Run();
+        FlipSprite();
     }
 
-    void ProcesarMovimiento()
+    void OnMove(InputValue value)
     {
-        float inputMovimiento = Input.GetAxis("Horizontal");
-        rigidBody.velocity = new Vector2(inputMovimiento*velocidad, rigidBody.velocity.y);
-        Orientacion(inputMovimiento);
+         moveInput = value.Get<Vector2>();
     }
 
-    void Orientacion(float inputMovimiento)
+    void Run()
     {
-        if((mirandoDerecha = true && inputMovimiento<0)||(mirandoDerecha=false && inputMovimiento>0)  )
+        //Inicia running
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y); //y queda en 0 / x se multiplica runspeed
+        myRigidbody.velocity = playerVelocity;
+
+        //Cambia animacion de idle a running
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x)> Mathf.Epsilon;
+        myAnimator.SetBool("isRunning",playerHasHorizontalSpeed);
+    }
+
+    void OnJump (InputValue value) 
+    {
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}  //si el boxcollider no esta tocando el layer Ground que siga al siguiente if
+        if(value.isPressed)
         {
-            mirandoDerecha=!mirandoDerecha;
-            transform.localScale = new Vector2(-transform.localScale.x,transform.localScale.y);
+            myRigidbody.velocity += new Vector2(0f,jumpSpeed);
+        }
+    }
+    void FlipSprite()
+    {   
+        //bool playertienevelocidadhorizontal?
+        //Mathf.Abs regresa el valor absoluto de f
+        //Mathf.Sign regresa el signo de f: regresa un 1 si f es positivo y un -1 si f es negativo
+        //Mathf.Epsilon el valor mas pequeno que un float puede tener diferente a 0. Usado para no poner 0
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x)> Mathf.Epsilon;
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x),1f);
         }
     }
 }
